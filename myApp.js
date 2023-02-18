d4702a44d28f9cf5fede6a1802be4194e2a414cb
Terbeche Mustapha <termus96@gmail.com>
2023-02-18 23:11:39 +0100

    send html file as response

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+absolutePath = __dirname + "/views/index.html";
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
