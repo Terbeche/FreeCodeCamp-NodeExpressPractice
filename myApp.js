4a0b31a3b96860976d541591622ca2e2085a2d42
Terbeche Mustapha <termus96@gmail.com>
2023-02-19 20:11:46 +0100

    Add styel to the html page

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 let express = require("express");
 let app = express();
+// const path = require("path");
 
 absolutePath = __dirname + "/views/index.html";
+
+stylePath = __dirname + "/public/";
+// app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(stylePath));
 
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
-
 module.exports = app;
